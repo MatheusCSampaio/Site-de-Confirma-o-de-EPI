340d04b8bcfa7c9e0f7a91e46e619fb8fa912c00
Matheus Cavalcante Sampaio <matheusampaio098@gmail.com>
2026-07-14 11:34:08 -0300

    Add files via upload

--- Estrutura-Site/logica.js
+++ Estrutura-Site/logica.js
@@ -1,7 +1,7 @@
             //Peguei o quadro de assinatura e o botão de limpar do HTML
 const canvas = document.getElementById("quadrado-assinatura");
 const botãoLimpar = document.getElementById("botão-limpar");
-const formulario = document.getElementById("formulario-de-epi");
+const formulario = document.getElementById("formulario-epi");
             //Defini o contexto do desenho para duas dimensões
 const contexto = canvas.getContext("2d");
             //Agora eu criei uma variável para saber se o usuário está desenhando ou não
@@ -33,7 +33,7 @@ function desenhar(evento) {
 
             //Configuração do traço da caneta.
     contexto.lineWidth = 2; //Grossura da linha
-    contexto.linecap = "round"; //Ponta da caneta mais arredondada
+    contexto.lineCap = "round"; //Ponta da caneta mais arredondada
     contexto.strokeStyle = "#000000"; //Cor
             //Faz o desenho acontecer na posição correta.
     contexto.lineTo(posiçãoX, posiçãoY);
@@ -60,15 +60,25 @@ botãoLimpar.addEventListener("click", limparTela);
 function enviarFormulario(evento) {
     evento.preventDefault(); //Essa função impede a página de recarregar e sumir com os dados.
 
-    alert("Botão clicado com sucesso!");
-
             //1- Vou pegar os valores do usuário
     const nome = document.getElementById("nome-funcionario").value;
-    const epi = document.getElementById("lista-epis").value;
+
+    const selectEpi = document.getElementById("Lista-epis");
+    const episSelecionados = Array.from(selectEpi.selectedOptions).map(opcao => opcao.text); //"selectEpi.selectedOptions" O JavaScript olha para a lista e cria uma coleção apenas com os itens que o usuário deixou azulzinho (selecionados).
+            //Pega a nova quantidade que um funcionário digitou
+    const quantidade = document.getElementById("quantidade-epi").value;
+
             //2- Transformar o desenho do canvas em um texto.
     const imagemAssinatura = canvas.toDataURL();
 
-    alert("Dados capturados com sucesso!:\Nome: " + nome + " \nEPI: " + epi);
+            //3- Alerta para confirmar o envio para o email.
+    alert("Dados enviados com sucesso!:\nNome: " + nome + " \nEPIs: " + episSelecionados.join(", ") + " \nQuantidade: " + quantidade);
+
+            //4- Resetar o formulário após o envio.
+    formulario.reset();
+
+            //5- Limpar a tela após a conclusão.
+    limparTela();
 }
             //Agora aqui é para o formulario ouvir o envio e chamar a função acima
 formulario.addEventListener("submit", enviarFormulario);
